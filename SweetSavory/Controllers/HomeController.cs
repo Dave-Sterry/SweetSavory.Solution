43ce6e287c6ee5b071406a14b59647c4fccf31ee
David Sterry <sterry.david@gmail.com>
2021-01-15 13:46:12 -0800

    add Dictionary to home controller index

--- SweetSavory/Controllers/HomeController.cs
+++ SweetSavory/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using SweetSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SweetSavory.Controllers
 {
   public class HomeController : Controller
   {
+
+    private readonly SweetSavoryContext _db;
+    public HomeController(SweetSavoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      Dictionary<object, object> model = new Dictionary<object, object>();
+      List<Flavor> Flavors = _db.Flavors.ToList();
+      List<Treat> Treats = _db.Treats.ToList();
+      model.Add("flavors", flavors);
+      model.Add("treats", treats);
       return View();
     }
   }
